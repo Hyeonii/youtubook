38c8820b7324c057c3e396ec0c40f08c45d7c004
Nahyeon Kim <nahyeonkim@d-ncomm.ai>
2023-04-14 12:48:19 +0900

    Feat: Add Youtube search feature

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ const app = express();
 const path = require('path');
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const axios = require('axios');
+const iconv = require('iconv-lite');
 const fs = require('fs');
 
 app.use(express.static('screens'));
@@ -30,7 +32,7 @@ io.on('connection', (socket) => {
     socket.on('share message', (msg) => {
         io.emit('share message', `${msg}`);
     })
-
+    
     socket.on('chat message', (msg) => {
         // Starting the process of converting a YouTube video to MP4.
         const regex = /https:\/\/www\.youtube\.com\/(watch\?v=|shorts\/)[a-zA-Z0-9]+/
@@ -41,22 +43,23 @@ io.on('connection', (socket) => {
             return
         }
         const youtubeUrl = regexMatched[0];
-        const convertToWav = spawn('python', [__dirname + '/python/convertToMP4.py', youtubeUrl]);
+        const convertToMP4 = spawn('python', [__dirname + '/python/convertToMP4.py', youtubeUrl]);
 
         // When Converting Video to MP4 is successed.
-        convertToWav.stdout.on('data', async function(data) {
+        convertToMP4.stdout.on('data', async function(data) {
             const mp4CreatedTime = new Date();
             const mp4CreatedTimeString = mp4CreatedTime.toLocaleTimeString();
             socket.emit('chat message', `${type}) MP4 file extraction successful. ${mp4CreatedTimeString}`);
+
             // Convert the Buffer object to a regular string
             const videoTitle = data.toString('utf-8').trim();
             const wavFilePath = path.join(ROOT_DIR, 'public', 'wav', videoTitle);
-
+            
             // Starting the process of converting a MP4 file to txt and changing the conversion method by type.
             const downloadFolderPath = ROOT_DIR + '\\public\\txt';
             const convertToText = type === 'API' 
                 ? spawn('python', [__dirname + '/python/convertToText.py', wavFilePath]) 
-                : spawn('whisper', [wavFilePath, '--output_dir', downloadFolderPath, '--output_format', 'txt'], {
+                : spawn('whisper', [wavFilePath, '--output_dir', downloadFolderPath, '--output_format', 'txt', '--word_timestamps'], {
                     env: {
                       PYTHONIOENCODING: 'utf-8'
                     }
@@ -95,12 +98,59 @@ io.on('connection', (socket) => {
                 const error = data.toString('utf-8').trim();
                 console.log(`${type}) Text file conversion has failed.\n${error}`)
                 socket.emit('chat message', `${type}) Text file conversion has failed.`);
-            });
-
+            });         
         });
         
-        convertToWav.stderr.on('data', function(data) {
+        convertToMP4.stderr.on('data', function(data) {
             socket.emit('chat message', `${type}) MP4 file extraction failed.`);
+        });
+    });
+
+    socket.on('search video', async (keyword) => {
+        console.log('keyword: ', keyword)
+
+        await axios.get(`https://www.youtube.com/results?search_query=${keyword}`, {
+            responseType: 'arraybuffer',
+            headers: {
+              'Content-Type': 'text/plain;charset=UTF-8',
+            },
+        })
+        .then(response => {
+            const decodedData = iconv.decode(Buffer.from(response.data), 'UTF-8');
+            // Saving decodedData in root directory for test.
+            // const randomString = Math.random().toString(12).substring(2, 6);
+
+            // fs.writeFile(`${keyword}_${randomString}.json`, decodedData, (err) => {
+            //     if (err) throw err;
+            //     console.log('The file has been saved!');
+            // });
+            const videoTitle = /"title":\{"runs":\[\{"text":"(.*?)"}]/
+            const videoUrl = /(\/(watch\?v=|shorts\/)[\w-]+\\?[\w-=%]+)"/
+            const thumbnailUrl = /\"videoId\"\:\"[\w-]+\"\,\"thumbnail\"\:\{\"thumbnails\"\:\[\{\"url\"\:\"([\w-=:.?/\\]+)"/
+            const views = /"runs":\[{"text":"([\d\.]+[백|천|만^"]?)"|조회수\s([\d\.]+[백|천|만^"]?)회"/
+            const nickName = /"longBylineText":\{"runs":\[\{"text":"(.*?)",/
+            const runningTime = /"lengthText":\{"accessibility":\{"accessibilityData":\{"label":"(.*?)"}},/
+        
+            const printData = [];
+            const videoInfoArr = decodedData.split('{"videoRenderer":')
+            videoInfoArr.shift();
+
+            videoInfoArr.map((info, idx) => {
+                const infoObj = new Object;
+                infoObj.title = info.match(videoTitle)[1];
+                infoObj.videoUrl = info.match(videoUrl)[1];
+                infoObj.thumbnailUrl = info.match(thumbnailUrl)[1];
+                infoObj.views = info.match(views)[1] === undefined ? info.match(views)[2] + '회' : info.match(views)[1] + '회';
+                infoObj.nickName = info.match(nickName)[1];
+                infoObj.runningTime = info.match(runningTime) === null ? '실시간' : info.match(runningTime)[1];
+
+                printData.push(infoObj);
+            })
+
+            socket.emit('search result', printData);
+        })
+        .catch(error => {
+            console.log(error);
         });
     });
 
